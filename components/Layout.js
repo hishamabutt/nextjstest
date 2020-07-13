c9c398d8d26b9214816efaacd5401e2402f1279b
Hisham <hishambutt9@gmail.com>
2020-07-13 20:12:22 +0500

    keywords, description and title

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,19 @@
 import NavbarComponent from './Navbar';
+import Head from 'next/head';
 
 const Layout = (props) => (
   <div>
+    <Head>
+      <title>Country Details</title>
+      <meta
+        name='description'
+        content='Next.js Testing App which show few countries details.'
+      />
+      <meta
+        name='keywords'
+        content='Next.js,SSR,Routing,Nextjs Roution,Countries alpha2code,alpha2Code,Countries details,Countries captial'
+      ></meta>
+    </Head>
     <NavbarComponent />
     {props.children}
   </div>
